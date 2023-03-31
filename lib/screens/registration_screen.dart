598aae76fe4eab6622da7bc15df653007751475c
chiranjeev kundu <chiranjeevkundu2000@gmail.com>
2023-03-31 23:52:45 +0530

    completed

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,11 +1,150 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter/material.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:loading_overlay/loading_overlay.dart';
+// import 'package:settyl_demo/constants/constants.dart';
+// import 'package:settyl_demo/components/rounded_button.dart';
+// import 'dart:core';
+
+// import 'package:settyl_demo/screens/user_screen.dart';
+
+// class RegistrationScreen extends StatefulWidget {
+//   static const String id = 'registration_screen';
+
+//   @override
+//   _RegistrationScreenState createState() => _RegistrationScreenState();
+// }
+
+// class _RegistrationScreenState extends State<RegistrationScreen> {
+//   final _auth = FirebaseAuth.instance;
+//   final _firestore = FirebaseFirestore.instance;
+//   late String email;
+//   late String password;
+//   late String fullName;
+//   late String address;
+//   bool showSpinner = false;
+
+//   // Future addUserDetails() async {
+//   //   await _firestore.collection('messages').doc(_auth.currentUser!.uid).set({
+//   //     'email': email,
+//   //     'password': password,
+//   //     'fullName': fullName,
+//   //     'address': address,
+//   //   });
+//   // }
+//   Future<void> addUserDetails() async {
+//     final user = _auth.currentUser;
+//     await _firestore.collection('users').doc(user!.uid).set({
+//       'email': email,
+//       'password': password,
+//       'fullName': fullName,
+//       'address': address,
+//     });
+//   }
+
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: LoadingOverlay(
+//         isLoading: showSpinner,
+//         child: Padding(
+//           padding: EdgeInsets.symmetric(horizontal: 24.0),
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             crossAxisAlignment: CrossAxisAlignment.stretch,
+//             children: <Widget>[
+//               Hero(
+//                 tag: 'logo',
+//                 child: Container(
+//                   height: 200.0,
+//                   child: Image.asset('images/logo.webp'),
+//                 ),
+//               ),
+//               const SizedBox(
+//                 height: 48.0,
+//               ),
+//               TextField(
+//                 keyboardType: TextInputType.emailAddress,
+//                 onChanged: (value) {
+//                   fullName = value;
+//                 },
+//                 decoration: kTextFieldDecoration.copyWith(
+//                     hintText: 'Enter Your Full Name'),
+//               ),
+//               const SizedBox(
+//                 height: 8.0,
+//               ),
+//               TextField(
+//                 keyboardType: TextInputType.emailAddress,
+//                 onChanged: (value) {
+//                   email = value;
+//                 },
+//                 decoration:
+//                     kTextFieldDecoration.copyWith(hintText: 'Enter Your Email'),
+//               ),
+//               const SizedBox(
+//                 height: 8.0,
+//               ),
+//               TextField(
+//                 keyboardType: TextInputType.emailAddress,
+//                 onChanged: (value) {
+//                   address = value;
+//                 },
+//                 decoration: kTextFieldDecoration.copyWith(
+//                     hintText: 'Enter Your Address'),
+//               ),
+//               const SizedBox(
+//                 height: 8.0,
+//               ),
+//               TextField(
+//                 obscureText: true,
+//                 onChanged: (value) {
+//                   password = value;
+//                 },
+//                 decoration: kTextFieldDecoration.copyWith(
+//                     hintText: 'Enter Your Password'),
+//               ),
+//               const SizedBox(
+//                 height: 24.0,
+//               ),
+//               RoundedButton(
+//                 colour: Colors.blueAccent,
+//                 title: 'Register',
+//                 onPressed: () async {
+//                   setState(() {
+//                     showSpinner = true;
+//                   });
+//                   try {
+//                     final newUser = await _auth.createUserWithEmailAndPassword(
+//                         email: email, password: password);
+//                     addUserDetails();
+//                     if (newUser != null) {
+//                       Navigator.pushNamed(context, UserScreen.id);
+//                     }
+//                     setState(() {
+//                       showSpinner = false;
+//                     });
+//                   } catch (e) {
+//                     print(e);
+//                   }
+//                 },
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:loading_overlay/loading_overlay.dart';
-import 'package:settyl_demo/constants/constants.dart';
 import 'package:settyl_demo/components/rounded_button.dart';
-import 'dart:core';
-
+import 'package:settyl_demo/constants/constants.dart';
 import 'package:settyl_demo/screens/user_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
@@ -24,8 +163,8 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   late String address;
   bool showSpinner = false;
 
-  Future addUserDetails() async {
-    await _firestore.collection('messages').add({
+  Future<void> addUserDetails(String email,String password,String fullName,String address) async {
+    await _firestore.collection('messages').doc().set({
       'email': email,
       'password': password,
       'fullName': fullName,
@@ -109,8 +248,8 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                   try {
                     final newUser = await _auth.createUserWithEmailAndPassword(
                         email: email, password: password);
-                    addUserDetails();
                     if (newUser != null) {
+                      await addUserDetails(email,password,fullName,address);
                       Navigator.pushNamed(context, UserScreen.id);
                     }
                     setState(() {
--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:settyl_demo/screens/googlemaps_screen.dart';
 import 'package:settyl_demo/screens/polygone_screen.dart';
 
 import '../components/rounded_button.dart';
+import '../constants/constants.dart';
+import 'login_screen.dart';
 
 class UserScreen extends StatefulWidget {
   static const String id = 'user_screen';
@@ -12,8 +16,60 @@ class UserScreen extends StatefulWidget {
 }
 
 class _UserScreenState extends State<UserScreen> {
+  final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
+  late User loggedInUser;
+  late String fullName = '';
+  late String address = '';
+  bool showSpinner = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    final user = _auth.currentUser;
+    if (user != null) {
+      loggedInUser = user;
+      getUserDetails();
+    } else {
+      Navigator.pushNamed(context, LoginScreen.id);
+    }
+  }
+
+  void getUserDetails() async {
+    final userSnapshot =
+        await _firestore.collection('updated').doc(loggedInUser.uid).get();
+    if (userSnapshot.exists) {
+      final user = userSnapshot.data() as Map<String, dynamic>;
+      setState(() {
+        fullName = user['fullName'];
+        address = user['address'];
+      });
+    }
+  }
+
+  Future<void> updateUserDetails() async {
+    await _firestore.collection('updated').doc(loggedInUser.uid).set({
+      'fullName': fullName,
+      'address': address,
+    }, SetOptions(merge: true));
+  }
+
+  // Future<void> addUserDetails(String userId) async {
+  //   await _firestore.collection('users').doc(userId).set({
+  //     // 'email': email,
+  //     // 'password': password,
+  //     'fullName': fullName,
+  //     'address': address,
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
     return DefaultTabController(
       length: 2,
       child: Scaffold(
@@ -44,9 +100,139 @@ class _UserScreenState extends State<UserScreen> {
         ),
         body: TabBarView(
           children: [
-            Center(
-              child: Text('User Details'),
+            // StreamBuilder<DocumentSnapshot>(
+            //   stream: FirebaseFirestore.instance
+            //       .collection('users')
+            //       .doc(user!.uid)
+            //       .snapshots(),
+            //   builder: (context, snapshot) {
+            //     if (!snapshot.hasData) {
+            //       return Center(
+            //         child: CircularProgressIndicator(),
+            //       );
+            //     }
+            //     final userData = snapshot.data!.data() as Map<String, dynamic>;
+            //     return Column(
+            //       mainAxisAlignment: MainAxisAlignment.center,
+            //       crossAxisAlignment: CrossAxisAlignment.stretch,
+            //       children: <Widget>[
+            //         Text(
+            //           'Full Name: ${userData['fullName']}',
+            //           textAlign: TextAlign.center,
+            //         ),
+            //         SizedBox(
+            //           height: 8.0,
+            //         ),
+            //         Text(
+            //           'Address: ${userData['address']}',
+            //           textAlign: TextAlign.center,
+            //         ),
+            //         SizedBox(
+            //           height: 8.0,
+            // ),
+            //         Text(
+            //           'Email: ${userData['email']}',
+            //           textAlign: TextAlign.center,
+            //         ),
+            //         SizedBox(
+            //           height: 24.0,
+            //         ),
+            //         RoundedButton(
+            //           colour: Colors.blueAccent,
+            //           title: 'Edit Details',
+            //           onPressed: () {
+            //             // Navigate to a screen where the user can edit their details
+            //           },
+            //         ),
+            //       ],
+            //     );
+            //   },
+            // ),
+            Scaffold(
+              backgroundColor: Colors.white,
+              body: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 48.0,
+                    ),
+                    Text(
+                      'User Details',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(
+                      height: 48.0,
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      onChanged: (value) {
+                        fullName = value;
+                      },
+                      decoration: kTextFieldDecoration.copyWith(
+                          hintText: 'Enter Your Full Name'),
+                      controller: TextEditingController(text: fullName),
+                    ),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      onChanged: (value) {
+                        address = value;
+                      },
+                      decoration: kTextFieldDecoration.copyWith(
+                          hintText: 'Enter Your Address'),
+                      controller: TextEditingController(text: address),
+                    ),
+                    const SizedBox(
+                      height: 24.0,
+                    ),
+                    RoundedButton(
+                      colour: Colors.blueAccent,
+                      title: 'Save',
+                      onPressed: () async {
+                        setState(() {
+                          showSpinner = true;
+                        });
+                        try {
+                          // await addUserDetails(user!.uid);
+                          await updateUserDetails();
+                          setState(() {
+                            showSpinner = false;
+                          });
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('User details saved'),
+                            ),
+                          );
+                        } catch (e) {
+                          print(e);
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 24.0,
+                    ),
+                    RoundedButton(
+                      colour: Colors.redAccent,
+                      title: 'Log Out',
+                      onPressed: () {
+                        _auth.signOut();
+                        Navigator.pushNamed(context, LoginScreen.id);
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ),
+
             Center(
               child: Column(
                 children: [
